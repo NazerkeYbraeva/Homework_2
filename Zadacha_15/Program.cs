2c09d42e812da653351edf9843f15b420c67e559
Nazerke <ybraeva.n@gmail.com>
2023-09-22 10:36:02 +0600

    Zadacha 15

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую
+// день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+bool FindWeekEnd(int num)
+{
+  int we = 6 & 7;
+  if(num < we)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int num = new Random().Next(1, 7 + 1);
+Console.WriteLine(num);
+bool result = FindWeekEnd(num);
+Console.WriteLine(result);
