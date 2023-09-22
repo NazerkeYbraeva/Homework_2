97be8928a3c00166cd7a5bd7e6c600f4ad43e32b
Nazerke <ybraeva.n@gmail.com>
2023-09-22 10:27:15 +0600

    Zadacha 10

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+void MiddleDigit()
+{
+  int n = new Random().Next(100, 999 + 1);
+  Console.WriteLine(n);
+  int num = (n / 10) % 10;
+  Console.WriteLine(num);
+}
+
+MiddleDigit();
