4f03f57c2a152ceefaeddb43b94499990b977d70
Nazerke <ybraeva.n@gmail.com>
2023-09-22 10:32:36 +0600

    Zadacha 13

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+void FindThirdNum()
+{
+  Console.Write("Input num: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+  int third = num % 10;
+  if(num < 99 + 1)
+  {
+    Console.WriteLine("Третьей цифры нет");
+  }
+  else
+  {
+    Console.WriteLine(third);
+  }
+}
+
+FindThirdNum();
